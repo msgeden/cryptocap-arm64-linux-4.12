ff524cf1ce84c9792a19057bc96c45fbeebfdf27
Munir Geden <munirgeden@hotmail.com>
2025-04-12 10:28:21 +0100

    test

--- fs/read_write.c
+++ fs/read_write.c
@@ -640,7 +640,7 @@ SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count) {
             //normal read 
             ret = vfs_read(f.file, buf, count, &pos);
             if (ret >= 0)
-                file_pos_write(f.file, pos);
+                file_pos_write(f.file, pos);  
             fdput_pos(f);
         }
     }
